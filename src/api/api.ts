506db42f0249724d0a728ff5f0c9ddd5de078e3b
Juanjo <juanjdiazjd@gmail.com>
2022-07-12 03:55:24 -0300

    minor fixes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,26 +1,5 @@
-import {create, TIMEOUT_ERROR} from 'apisauce';
+import {create} from 'apisauce';
 import apiConfig from './services/config';
-
-const requestTimeout = (promise: any, axiosConfig: any, reqConfig: any) => {
-  const {timeout} = apiConfig;
-  const config = {...axiosConfig, ...(reqConfig || {})};
-  const duration = (parseInt(config.timeout, 10) || timeout) + 1e3;
-  const result = {
-    ok: false,
-    problem: TIMEOUT_ERROR,
-    originalError: 'Request Timeout Error',
-    data: null,
-    status: null,
-    headers: null,
-    config,
-    duration,
-  };
-  const timeoutPromise = new Promise(resolve =>
-    setTimeout(resolve, duration, result),
-  );
-
-  return Promise.race([timeoutPromise, promise]);
-};
 
 const buildApi = (config = apiConfig) => {
   const api = create({
@@ -28,26 +7,16 @@ const buildApi = (config = apiConfig) => {
     ...config,
     headers: {...apiConfig.headers, ...config.headers},
   });
-  const {
-    axiosInstance: {defaults},
-    get,
-    delete: del,
-    head,
-    post,
-    put,
-    patch,
-    link,
-    unlink,
-  } = api;
+  const {get, delete: del, head, post, put, patch, link, unlink} = api;
 
   api.get = (...args) => get(...args);
-  api.delete = (...args) => requestTimeout(del(...args), defaults, args[2]);
-  api.head = (...args) => requestTimeout(head(...args), defaults, args[2]);
+  api.delete = (...args) => del(...args);
+  api.head = (...args) => head(...args);
   api.post = (...args) => post(...args);
-  api.put = (...args) => requestTimeout(put(...args), defaults, args[2]);
-  api.patch = (...args) => requestTimeout(patch(...args), defaults, args[2]);
-  api.link = (...args) => requestTimeout(link(...args), defaults, args[2]);
-  api.unlink = (...args) => requestTimeout(unlink(...args), defaults, args[2]);
+  api.put = (...args) => put(...args);
+  api.patch = (...args) => patch(...args);
+  api.link = (...args) => link(...args);
+  api.unlink = (...args) => unlink(...args);
   return api;
 };
 
--- src/components/Text/utils/index.ts
+++ src/components/Text/utils/index.ts
@@ -1,3 +1,5 @@
+import {TextType, textValues} from '../TextView';
+
 export const findInValues = (type: TextType) => {
   return textValues.find(text => text.type === type);
 };
